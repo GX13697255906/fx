4e62c3d23e431d43967a89bd8683798a644e3625
xun.guo <2594226416@qq.com>
2022-01-12 17:43:45 +0800

    完成路由从数据库加载功能

--- fx-common/src/main/java/com/fx/cloud/common/constants/Constants.java
+++ fx-common/src/main/java/com/fx/cloud/common/constants/Constants.java
@@ -5,8 +5,14 @@ package com.fx.cloud.common.constants;
  */
 public class Constants {
 
+    /**
+     * 网关服务名称
+     */
     public final static String GATE_WAY_SERVER = "fx-gateway-server";
 
+    /**
+     * nacos获取服务信息时，判断服务健康节点数量字段
+     */
     public final static String NACOS_HEALTHY_KEY = "nacos.healthy";
 
 }
